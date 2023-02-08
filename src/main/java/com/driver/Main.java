5e1b47c6835401a0c3da55197d521a1f22ea54a6
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-02-08 16:12:30 +0530

    my 1st commit in this project

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A {
+        String meth() {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A {
+        String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[]args){
+        B b1= new B();
+        b1.meth();
+
+        B b2= new B();
+        b2.meth();
+    }
 }
